761765034910a756d55106acba4d9abb028cf39f
XilefEel <felixlee175@gmail.com>
2025-10-24 00:34:35 +0700

    updated images and projects section, and added github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://XilefEel.github.io",
+  base: "/personal-website",
   vite: {
     plugins: [tailwindcss()],
   },
